501733c8a6ccc10628b240a56356d8aa70c68b3f
dipti58 <biswaldipti757@gmail.com>
2025-11-28 00:21:30 +0530

    Add files via upload

---
+++ Test10.java
@@ -0,0 +1,16 @@
+package ArrayProgram;
+
+//16)find the maximum value in an array.
+
+public class Test10 {
+    public static void main(String[] args) {
+        int[] a={10,4,6,7,9,4,8,2};
+        int min=a[0];
+        for(int i=1; i<a.length; i++){
+            if(min>a[i]){
+                min=a[i];
+            }
+        } 
+        System.out.println(min);
+    }
+}
