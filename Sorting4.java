42b2a5d305be0084f68c906266ad924a98a09bea
dipti58 <biswaldipti757@gmail.com>
2025-12-05 23:27:24 +0530

    Add files via upload

---
+++ Sorting4.java
@@ -0,0 +1,21 @@
+package ArrayProgram;
+
+// sort half of the element in desending order
+
+public class Sorting4 {
+     public static void main(String[] args) {
+        int[] a={10,3,5,7,6,2,12,16,4,9};
+
+        for(int i=0; i<a.length; i++){
+            for(int j=i+1; j<a.length/2; j++)
+            {
+                if(a[i]<a[j]){
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+            System.out.println(a[i]);
+        }
+    }
+}
